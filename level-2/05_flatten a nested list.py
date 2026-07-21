c9878bfeff4aee8cc73da72bed3307d880f101cd
Ankita Singh <pingme@ankita@gmail.com>
2026-07-21 23:19:39 +0530

    Added flatten nested list solution

---
+++ level-2/05_flatten a nested list.py
@@ -0,0 +1,16 @@
+def flatten(lst):
+    result = []
+
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+
+    return result
+
+
+nested = [1, [2, 3], [4, [5, 6]], 7]
+
+print("Original List:", nested)
+print("Flatten List:", flatten(nested))
